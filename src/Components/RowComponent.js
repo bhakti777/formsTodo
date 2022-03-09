6c36f431d43d80d6a4508c3f53f8c7afa57785ed
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2022-03-10 00:01:10 +0530

    Handling completed task

---
+++ src/Components/RowComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+import CloseButton from "react-bootstrap/CloseButton";
+
+const RowComponent = ({ title, onSelect, index, checked }) => {
+
+  return (
+    <>
+      <ListGroup.Item>
+        <div>
+          <Row>
+
+            <Col xs={1}>
+              <input
+                checked={checked}
+                type="checkbox"
+                onClick={() => {
+                  onSelect(index)
+                }}
+                style={{ height: "20px", width: "20px" }}
+              />
+            </Col>
+            <Col xs={10}>{title}</Col>
+            <Col
+              xs={1}
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <CloseButton />
+            </Col>
+          </Row>
+        </div>
+      </ListGroup.Item>
+    </>
+  );
+};
+
+export default RowComponent;
--- src/Pages/TodoPage.js
+++ src/Pages/TodoPage.js
@@ -8,94 +8,112 @@ import ListGroup from "react-bootstrap/ListGroup";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormLabel from "react-bootstrap/FormLabel";
 import FormControl from "react-bootstrap/FormControl";
-import ListComponent from "../Components/ListComponent";
-import CheckedListComponent from "../Components/CheckedListComponent";
+import RowComponent from "../Components/RowComponent";
+
+
+// 1. CompleteTask []
+// 2. Not completed task []
+// 3. New add -> Not completed task
+// 4. To remove from not complete array and add it into complete array
+// 5. UnCheck completed -> non completed
 
 const TodoPage = () => {
-  const [todoList, setTodoList] = useState(["Learn Flute"]);
 
-  const AddTodoItem = () => {
-    const newTodoItem = document.querySelector("#textval").value;
-    setTodoList([...todoList, newTodoItem]);
-    console.log("todoItem=>", todoList);
+  const [userInput, setUserInput] = useState("");
+  const [completeTaskList, setCompletedTaskList] = useState(["Learn Flute"]);
+  const [notCompletedTaskList, setNotCompletedTaskList] = useState(["Done Flute"]);
+
+
+  const handleTextOnChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
+  const addTodoItem = () => {
+    setNotCompletedTaskList([...notCompletedTaskList, userInput]);
+    setUserInput("");
   };
 
-  const deleteItemFromList = (indexToDelete) => {
-    const todoListClone = [...todoList];
-    
-    todoListClone.splice(indexToDelete, 1);
-    setTodoList(todoListClone);
-  };
 
-  const handleCheckedList = (indexToMove) => {
-    let checkedItems = [...todoList];
-    checkedItems.splice(indexToMove, 1);
-    setTodoList(checkedItems);
-  };
+  const handleMarkAsCompleted = (index) => {
+    const notCompletedTaskListClone = [...notCompletedTaskList]; // Copy state variable
+    const completedTask = notCompletedTaskListClone[index]; // Get the task which needs to be marked as completed
+    setCompletedTaskList([...completeTaskList, completedTask]); // Add to completed task
+    notCompletedTaskListClone.splice(index, 1) // Remove the task from not completed
+    setNotCompletedTaskList(notCompletedTaskListClone)
+
+  }
+
+  const handleMarkAsNotCompleted = (index) => {
+
+  }
+
+  console.log("completeTaskList =>", completeTaskList);
+  console.log("notCompletedTaskList =>", notCompletedTaskList);
 
   return (
     <Fragment>
       <Container fluid className="todo-Container">
         <h1 className="Heading">To-Do List</h1>
-
-        <Form>
-          <FormLabel>
-            <b>Add Todo</b>
-          </FormLabel>
-          <Row>
-            <Col xs={6}>
-              <InputGroup className="mb-3">
-                <FormControl
-                  id="textval"
-                  placeholder="Add new todo"
-                  aria-label="Recipient's username"
-                  aria-describedby="basic-addon2"
-                />
-                <Button variant="outline-dark" onClick={AddTodoItem}>
-                  Add
+        <FormLabel>
+          <b>Add Todo</b>
+        </FormLabel>
+        <Row>
+          <Col xs={6}>
+            <InputGroup className="mb-3">
+              <FormControl
+                value={userInput}
+                id="textval"
+                onChange={handleTextOnChange}
+                placeholder="Add new todo"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-dark" onClick={addTodoItem}>
+                Add
                 </Button>
-              </InputGroup>
-            </Col>
-            <Col xs={6}></Col>
-          </Row>
-          <br />
+            </InputGroup>
+          </Col>
+          <Col xs={6}></Col>
+        </Row>
+        <br />
 
+        <Row>
+          <Col xs={6}>
+            <div>
+              <b>Not-Completed Tasks :</b>
+              <ListGroup>
+                {notCompletedTaskList.map((todoItem, index) => {
+                  return (
+                    <RowComponent
+                      checked={false}
+                      index={index}
+                      title={todoItem}
+                      onSelect={handleMarkAsCompleted}
+                    />
+                  );
+                })}
+              </ListGroup>
+            </div>
+          </Col>
 
+          <Col xs={6}>
+            <div className="deleteListDiv">
+              <b> Completed Tasks :</b>
+              <ListGroup>
+                {completeTaskList.map((todoItem) => {
+                  return (
+                    <RowComponent
+                      checked={true}
+                      title={todoItem}
+                      onSelect={handleMarkAsNotCompleted}
+                    />
+                  );
+                })}
+              </ListGroup>
+            </div>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col xs={6}>
-              <div>
-                <b>Not-Completed Tasks :</b>
-                <ListGroup>
-                  {todoList.map((todoItem) => {
-                    return (
-                      <ListComponent
-                        title={todoItem}
-                        onSelect={deleteItemFromList}
-                      />
-                    );
-                  })}
-                </ListGroup>
-              </div>
-            </Col>
-
-            <Col xs={6}>
-              <div className="deleteListDiv">
-                <b> Completed Tasks :</b>
-                <ListGroup>
-                  {todoList.map((checkedTask) => {
-                    return (
-                      <CheckedListComponent
-                        checkedTitle={checkedTask}
-                        onCheck={handleCheckedList}
-                      />
-                    );
-                  })}
-                </ListGroup>
-              </div>
-            </Col>
-          </Row>
-        </Form>
       </Container>
     </Fragment>
   );
